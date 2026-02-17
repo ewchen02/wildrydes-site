e4cc427f8a3c9522bc4efb4fc45db5defe849bb0
Nate Chen <ecosdevelopment@gmail.com>
2026-02-17 02:38:00 -0800

    updating config file cognito details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_IMgCmPCSI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5uthui8ksl0mdug09qkgkku5pg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-1' // e.g. us-east-2
+        userPoolId: 'us-west-1_IMgCmPCSI',
+        userPoolClientId: '5uthui8ksl0mdug09qkgkku5pg',
+        region: 'us-west-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
